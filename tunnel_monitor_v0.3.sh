e8c0ff3d30e0c108d531cd6177a0ce0e8391616d
Andrew-S-Nelson <andrewnelson2596@gmail.com>
2024-09-19 01:43:31 -0400

    Update tunnel_monitor_v0.3.sh

--- tunnel_monitor_v0.3.sh
+++ tunnel_monitor_v0.3.sh
@@ -4,13 +4,43 @@ NC='\033[0m' # No Color
 while [[ true ]]
 do
 	tunnel_pids=($(ss -ntlp | grep -Po "ssh\",pid=\d+,fd=5" | grep -Po "\d+," | cut -d',' -f1 | sort -u))
-	socket_pids=($(ss -ntlp | grep -Po "ssh\",pid=\d+,fd=\d+\)" | grep -Pv "fd=[45]\)" | grep -Po "\d+," | cut -d',' -f1 | sort -u))	
-	# get tunnels
+	socket_pids=($(ss -nap | grep "ssh\"" | grep -i "strLISTEN" | grep -Po "pid=\d+" | cut -d"=" -f2))	
+	
+	# all ssh sessions
+	ssh_list=()
+	ssh_sessions=$(ss -ntp | grep "ssh" | grep -Po "(\d{1,3}\.){3}\d{1,3}:\d+ .*pid=\d+" | sed -e 's/^/%/g')
+	i=0
+	z=2
+	while [[ true ]]; do
+		if [[ -z $(echo ${ssh_sessions} | cut -d"%" -f${z}) ]]; then
+			break
+		fi
+		session=$(echo ${ssh_sessions} | cut -d"%" -f${z})
+		src=$(echo ${session} | grep -Po "^[\d.]+:\d+")
+		dst=$(echo ${session} | grep -Po " [\d.]+:\d+ ")
+		pid=$(echo ${session} | grep -Po "pid=\d+" | cut -d"=" -f2)
+
+		ssh_list[${i}]="${src} --> ${dst} - PID ${pid}"
+		i=$[ $i + 1 ]
+		z=$[ $z + 1 ]
+	done
+
+	
+	# get traditional tunnels
 	tunnel_list=()
 	i=0
 	for item in ${tunnel_pids[*]}; do
 		process=$(ps -ef | grep -P "${item}" | grep -Po "ssh .*")
 		ssh=$(echo $process | grep -Po "ssh (\w+@)?([a-zA-Z]\w+|[\d\.]+) (-p ?\d+)?")
+
+		# grab attached ssh session
+		for ((x = 0 ; x < ${#ssh_list[@]} ; x++)); do
+			if [[ $(echo ${ssh_list[${x}]} | grep -c "PID ${item}") -ge 1 ]]; then
+				tunnel_list[${i}]="%SESSION: $(echo ${ssh_list[${x}]} | cut -d" " -f-3)"
+				ssh_list[${x}]=""
+				i=$[ $i + 1 ]
+			fi
+		done
 
 		# formatting for source to dest port
 		fwd_field=$(echo $process | grep -Po "\-[LR] ?\d+:([\d.]+|\w+):\d+" | cut -d" " -f2-)
@@ -24,7 +54,7 @@ do
 			tunnel_list[$[${i} + 1 ]]=$(echo -e "%${srcip}:${srcport} --> ${dstip}:${dstport}")
 			i=$[ $i + 2 ]
 		elif [[ -n $(echo $process | grep -Po "\-\w*D\w* ?9050") ]]; then
-			tunnel_list[$[${i} + 1 ]]=$(echo -e "%127.0.0.1:9050 --> DYNAMIC")
+			tunnel_list[$[${i} + 1 ]]=$(echo -e "%FORWARD: 127.0.0.1:9050 --> DYNAMIC")
 			i=$[ $i + 2 ]
 		fi	
 		fwd_field=""
@@ -42,6 +72,14 @@ do
 		# finds all local ports being forwarded via this master socket
 		forward_ports=$(ss -ntlp | grep "pid=${item}" | grep -Po "127.0.0.1:\d+" | cut -d":" -f2 | sed -e 's/^/%/g')
 		
+		# grab attached ssh session
+		for ((x = 0 ; x < ${#ssh_list[@]} ; x++)); do
+			if [[ $(echo ${ssh_list[${x}]} | grep -c "PID ${item}") -ge 1 ]]; then
+				socket_list[${i}]="%SESSION: $(echo ${ssh_list[${x}]} | cut -d" " -f-3)"
+				ssh_list[${x}]=""
+				i=$[ $i + 1 ]
+			fi
+		done
 		# finds the master socket file
 		socket_file=$(echo $ms | grep -Po '\-\w* [/\w]+' | cut -d" " -f2)
 		#echo "file ${socket_file}"
@@ -66,7 +104,7 @@ do
 				dstport=$(echo ${fwd_field} | cut -d":" -f3)
 
 				# output formatting
-				socket_list[${i}]="%${srcip}:${srcport} --> ${dstip}:${dstport}"
+				socket_list[${i}]="%FORWARD: ${srcip}:${srcport} --> ${dstip}:${dstport}"
 
 			elif [[ $search_port -eq 9050 ]]; then
 				srcip="127.0.0.1"
@@ -76,30 +114,25 @@ do
                                 socket_list[${i}]="%${srcip}:${srcport} --> DYNAMIC"
                                 i=$[ $i + 1 ]
 			else
-				socket_list[${i}]="%127.0.0.1:${search_port} --> UNK"
+				socket_list[${i}]="%FORWARD: 127.0.0.1:${search_port} --> UNK"
 			fi
 			i=$[ $i + 1 ]
 			z=$[ $z + 1 ]
 		done
 	done
 	
-	# all ssh sessions
-	ssh_list=()
-	ssh_sessions=$(ss -ntp | grep "ssh" | grep -Po "(\d{1,3}\.){3}\d{1,3}:\d+ .*pid=\d+" | sed -e 's/^/%/g')
-	i=0
-	z=2
-	while [[ true ]]; do
-		if [[ -z $(echo ${ssh_sessions} | cut -d"%" -f${z}) ]]; then
-			break
+	# get non-tunnel ssh sessions	
+	z=0
+	nt_ssh_list=()
+	for ((i = 0 ; i < ${#ssh_list[@]} ; i++)); do
+		if [[ -z ${ssh_list[${i}]} ]]; then
+			continue
 		fi
-		session=$(echo ${ssh_sessions} | cut -d"%" -f${z})
-		src=$(echo ${session} | grep -Po "^[\d.]+:\d+")
-		dst=$(echo ${session} | grep -Po " [\d.]+:\d+ ")
-		pid=$(echo ${session} | grep -Po "pid=\d+" | cut -d"=" -f2)
-
-		ssh_list[${i}]="${src} --> ${dst} - PID ${pid}"
-		i=$[ $i + 1 ]
-		z=$[ $z + 1 ]
+		nt_ssh_list[${z}]=${ssh_list[${i}]}
+		pid=$(echo ${ssh_list[${i}]} | grep -Po "PID \d+" | cut -d" " -f2)
+		command=$(ps -efq ${pid} --no-headers | grep -Po "ssh .*")
+		nt_ssh_list[$[$z + 1]]="%COMMAND: ${command}"
+		z=$[$z + 2]
 	done
 
 	# PRINT BLOCK
@@ -120,12 +153,14 @@ do
 	done
 	echo -e "${NC}"
 
-	# print all ssh sessions
-	echo -e "All SSH sessions: ${CYAN}"
-	for ((i = 0 ; i < ${#ssh_list[@]} ; i++)); do
-		echo ${ssh_list[${i}]}
+	# print non-tunnel ssh sessions
+	echo -e "Non-tunnel ssh sessions: ${CYAN}"
+	for ((i = 0 ; i < ${#nt_ssh_list[@]} ; i++)); do
+		if [[ -z ${nt_ssh_list[${i}]} ]]; then
+			continue
+		fi
+		echo ${nt_ssh_list[${i}]} | sed -e 's/%/\t/g'
 	done
 	echo -e "${NC}"
-
-	sleep 1
+	sleep 0.5
 done
